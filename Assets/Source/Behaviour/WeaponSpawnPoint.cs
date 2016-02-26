b92eb2149ddaff08d1530c438d97b43cfca64fb0
Emilien REGENT <e.regent@owlient.eu>
2016-02-26 17:13:06 +0100

    A Respawn for weapons
    Change-Id: I95a581533ee6d3ce899b79f1491ee48bb88f37b3

--- Assets/Source/Behaviour/WeaponSpawnPoint.cs
+++ Assets/Source/Behaviour/WeaponSpawnPoint.cs
@@ -1,23 +1,52 @@
-﻿using UnityEngine;
+﻿using Simple.CustomType;
+using UnityEngine;
 
 namespace Simple.Behaviour
 {
+	/// <summary>
+	/// Handle behaviour of weapon spawn point
+	/// </summary>
     public class WeaponSpawnPoint : MonoBehaviour
     {
         [SerializeField]
         private GameObject[] _weapons = new GameObject[0];
+		[Range(0f, 60f)]
+		[SerializeField]
+		private float _cooldown = 0f;
 
-        private GameObject _body = null;
+		private GameObject _body = null;
         private Weapon _weapon = null;
+		private Timer _timer = null;
 
         public GameObject body { get { return _body; } }
         public Weapon weapon { get { return _weapon; } }
 
+		/// <summary>
+		/// Initialize this monobehaviour
+		/// </summary>
         private void Awake()
         {
             CreateWeapon();
         }
 
+		/// <summary>
+		/// Update this monobehaviour
+		/// </summary>
+		private void Update()
+		{
+			if(_timer != null && _timer.isFinished == true)
+			{
+				CreateWeapon();
+
+				_timer = null;
+			}
+		}
+
+		/// <summary>
+		/// Create a new weapon for the spawn point
+		/// </summary>
+		/// <param name="prefab">The prefab used to create the weapon</param>
+		/// <returns>The gameobject created</returns>
         public GameObject CreateWeapon(GameObject prefab = null)
         {
             if (prefab == null)
@@ -40,20 +69,41 @@ namespace Simple.Behaviour
             return _body;
         }
 
+		/// <summary>
+		/// Grab the current weapon for a specific player
+		/// </summary>
+		/// <param name="player">The player who grabbed the weapon</param>
+		private void GrabWeapon(PlayerGame player)
+		{
+			player.AddWeapon(_weapon);
+
+			GameObject.Destroy(_body);
+
+			_body = null;
+			_weapon = null;
+
+			_timer = new Timer(_cooldown);
+		}
+
+		/// <summary>
+		/// Detect when something collide with this trigger
+		/// </summary>
+		/// <param name="other">The other collider which collides</param>
         private void OnTriggerEnter(Collider other) {
             if (_weapon != null)
             {
                 PlayerGame player = other.GetComponent<PlayerGame>();
 
-                player.AddWeapon(_weapon);
-
-                GameObject.Destroy(_body);
-
-                _body = null;
-                _weapon = null;
+               if(player != null)
+				{
+					GrabWeapon(player);
+				}
             }
         }
 
+		/// <summary>
+		/// Draw a gizmo to see spawn points on scene
+		/// </summary>
         public void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
---
+++ Assets/Source/Type/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Simple.CustomType
+{
+	public class Timer
+	{
+		private float _time = 0f;
+		private float _duration = 0f;
+
+		public float time { get { return _time; } }
+		public float duration { get { return _duration; } }
+
+		public bool isFinished { get { return Time.time - _time > _duration; } }
+
+		public Timer(float duration)
+		{
+			_time = Time.time;
+			_duration = duration;
+		}
+	}
+}
